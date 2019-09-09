96bfe11bfad36fdc1bb5c830ae3e3667ba41cb10
Joseph <joseph.howard1995@gmail.com>
2019-09-09 14:22:16 -0400

    Add helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+import random
+print("Joseph Howard")
+random.seed()
+num1 = random.randrange(0, 100)
+num2 = random.randrange(0, 100)
+print(num1)
+print(num2)
+print("Sum =", num1 + num2)
+print("Average =", (num1 + num2)/2)
