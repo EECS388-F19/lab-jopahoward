8109bf83f71a669e21199ead519ba008d4f89201
jphow <jphow@umich.edu>
2019-09-09 14:37:39 -0400

    Update helloworld.py with randint

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,9 @@
 import random
 print("Joseph Howard")
 random.seed()
-num1 = random.randrange(0, 100)
-num2 = random.randrange(0, 100)
+num1 = random.randint(0, 100)
+num2 = random.randint(0, 100)
 print(num1)
 print(num2)
 print("Sum =", num1 + num2)
-print("Average =", (num1 + num2)/2)
+print("Average =", (num1 + num2)/2.0)
